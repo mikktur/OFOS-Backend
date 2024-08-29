2fd0ab0a32a93e9d34ebe10fc73287f696a72267
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-08-29 17:08:47 +0300

    UserService tests

---
+++ src/test/java/service/UserServiceTests.java
@@ -0,0 +1,106 @@
+package service;
+
+
+import ofos.dto.CreateUserRequestDTO;
+import ofos.entity.UserEntity;
+import ofos.repository.UserRepository;
+import ofos.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void getUserByIdTest() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(222L);
+        userEntity.setUsername("kissa2");
+        userEntity.setPassword("kissa2");
+        userEntity.setRole("kissa");
+
+        Mockito.when(userRepository.findByUserId(222L)).thenReturn(userEntity);
+
+        UserEntity returnedUserEntity = userService.getUserById(222L);
+
+        Assertions.assertEquals(userEntity, returnedUserEntity);
+        Assertions.assertEquals(userEntity.getUsername(), returnedUserEntity.getUsername());
+    }
+
+    @Test
+    public void getUserByUsernameTest() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(222L);
+        userEntity.setUsername("kissa2");
+        userEntity.setPassword("kissa2");
+        userEntity.setRole("kissa");
+
+        Mockito.when(userRepository.findByUsername("kissa2")).thenReturn(userEntity);
+
+        UserEntity returnedUserEntity = userService.getUserByUsername("kissa2");
+
+        Assertions.assertEquals(userEntity, returnedUserEntity);
+        Assertions.assertEquals(userEntity.getUserId(), returnedUserEntity.getUserId());
+    }
+
+    @Test
+    public void getAllUsersTest() {
+        List<UserEntity> users = new ArrayList<>();
+        UserEntity kissa = new UserEntity(33L, "ksis", "ksis", "kissa", true);
+        UserEntity kissa1 = new UserEntity(334L, "ksis22", "ksis22", "kissa", true);
+        users.add(kissa);
+        users.add(kissa1);
+
+
+        Mockito.when(userRepository.findAll()).thenReturn(users);
+
+        List<UserEntity> returnedUsers= userService.getAllUsers();
+
+        Assertions.assertEquals(users, returnedUsers);
+        Assertions.assertEquals(returnedUsers.size(), users.size());
+    }
+
+    @Test
+    public void createUserTest() {
+        String username = "testUser";
+        String password = "testPassword";
+
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO();
+        createUserRequestDTO.setUsername(username);
+        createUserRequestDTO.setPassword(password);
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        userEntity.setPassword(password);
+
+        Mockito.when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
+        Mockito.when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+
+        UserEntity returnedUserEntity = userService.createUser(createUserRequestDTO);
+
+        Assertions.assertEquals(userEntity, returnedUserEntity);
+        Assertions.assertEquals(userEntity.getUsername(), returnedUserEntity.getUsername());
+    }
+
+}
