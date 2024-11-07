5274accd1d780326667d1442ab431d92a94c4220
mikktur <mikko.silenceri@gmail.com>
2024-11-07 12:39:36 +0200

    -add validation to ChangePasswordDTO

--- src/main/java/ofos/dto/ChangePasswordDTO.java
+++ src/main/java/ofos/dto/ChangePasswordDTO.java
@@ -1,5 +1,7 @@
 package ofos.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +10,9 @@ import lombok.Setter;
 public class ChangePasswordDTO {
 
     private String oldPassword;
-
+    @Size(min = 6, max = 20
+            , message = "Password must be between 6 and 20 characters")
+    @Pattern(regexp = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"|,.<>/?].*",
+            message = "Password must contain at least one special character")
     private String newPassword;
 }
